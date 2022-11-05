bd17dd9a3fa1a510936ac676494522815539c4bd
rahmadinaariyanti <rahmadinaariyanti30@gmail.com>
2022-11-05 10:56:35 +0700

    perubahan sintaks

---
+++ views/includes/footer.php
@@ -0,0 +1,11 @@
+</div>
+<!-- jQuery first, then Metro UI JS -->
+<script src="<?= $base_url ?>/assets/vendors/jquery/jquery-3.4.1.min.js"></script>
+<script src="<?= $base_url ?>/assets/vendors/chartjs/Chart.bundle.min.js"></script>
+<script src="<?= $base_url ?>/assets/vendors/qrcode/qrcode.min.js"></script>
+<script src="<?= $base_url ?>/assets/vendors/jsbarcode/JsBarcode.all.min.js"></script>
+<script src="<?= $base_url ?>/assets/vendors/ckeditor/ckeditor.js"></script>
+<script src="<?= $base_url ?>/assets/vendors/metro4/js/metro.min.js"></script>
+<script src="<?= $base_url ?>/assets/js/index.js"></script>
+</body>
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" class=" scrollbar-type-1 sb-cyan">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Metro 4 -->
+    <link rel="stylesheet" href="<?= $base_url ?>/assets/vendors/metro4/css/metro-all.min.css">
+    <link rel="stylesheet" href="<?= $base_url ?>/assets/css/index.css">
+
+    <title>Aplikasi Ecommers</title>
+
+    <script>
+        window.on_page_functions = [];
+    </script>
+</head>
+<body class="m4-cloak h-vh-100">
+<div data-role="navview" data-toggle="#paneToggle" data-expand="xl" data-compact="lg" data-active-state="true">
